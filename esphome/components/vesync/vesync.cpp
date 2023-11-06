85a136e9e1893ef085c7485028c8ac9d8db494da
William Heimbigner <william.heimbigner@gmail.com>
2023-11-06 16:59:53 -0600

    leeeeeeeroy jenkins initial commit

---
+++ esphome/components/vesync/vesync.cpp
@@ -0,0 +1,193 @@
+#include "vesync.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+  namespace vesync {
+
+  static const char *TAG = "vesync";
+
+  void vesync::setup() {
+  }
+
+  void vesync::loop() {
+    const uint32_t now = millis();
+    if (now - this->last_transmission_ >= 200) {
+      // last transmission too long ago. Reset RX index.
+      this->data_index_ = 0;
+    }
+    this->last_transmission_ = now;
+    while (this->available() != 0) {
+      this->read_byte(&this->data_[this->data_index_]);
+      auto check = this->check_byte_();
+      if (!check.has_value()) {
+        // finished
+        this->parse_data_();
+        this->data_index_ = 0;
+        this->last_update_ = now;
+      } else if (!*check) {
+        // wrong data
+        this->data_index_ = 0;
+      } else {
+        // next byte
+        this->data_index_++;
+      }
+    }
+  }
+
+void vesync::handle_char_(uint8_t c) {
+  // Append the character to the message buffer
+  this->rx_message_.push_back(c);
+}
+
+float vesync::get_setup_priority() const { return setup_priority::DATA; }
+optional<bool> vesync::check_byte_() {
+  uint8_t index = this->data_index_;
+  uint8_t byte = this->data_[index];
+
+  if (index == 0)
+    return byte == 0xA5;
+
+  if (index == 1)
+    return (byte == 0x12) || (byte == 0x22);
+// I think 0x12 = message from MCU, 0x22 = message TO MCU, but MCU seems to send 0x12 sometimes
+  if (index == 2)
+    return true;
+
+  uint8_t payload_length = uint8_t(this->data_[3]);
+//this->get_16_bit_uint_(3);
+//payload length might be uint16 not 8, but a payload > 32 bytes never happens
+  if (index == 3) {
+    if (payload_length < 4) {
+      ESP_LOGW(TAG, "vesync payload length too short! %d", payload_length);
+      return false;
+    }
+    return true;
+  }
+  if (index == 4) {
+    return byte == 0x00;
+  }
+
+  // start (16bit) + length (16bit) + DATA (payload_length-2 bytes) + checksum (16bit)
+  uint8_t total_size = 6 + payload_length;
+
+  if (index < total_size)
+    return true;
+
+  return {};
+}
+
+void vesync::parse_data_() {
+  /* todo
+      uint16_t pm_1_0_std_concentration = this->get_16_bit_uint_(4);
+      uint16_t pm_2_5_std_concentration = this->get_16_bit_uint_(6);
+      uint16_t pm_10_0_std_concentration = this->get_16_bit_uint_(8);
+
+      ESP_LOGD(TAG,
+               "Got PM1.0 Concentration: %u µg/m^3, PM2.5 Concentration %u µg/m^3, PM10.0 Concentration: %u µg/m^3",
+               pm_1_0_concentration, pm_2_5_concentration, pm_10_0_concentration);
+
+      if (this->pm_1_0_std_sensor_ != nullptr)
+        this->pm_1_0_std_sensor_->publish_state(pm_1_0_std_concentration);
+      if (this->pm_2_5_std_sensor_ != nullptr)
+        this->pm_2_5_std_sensor_->publish_state(pm_2_5_std_concentration);
+      if (this->pm_10_0_std_sensor_ != nullptr)
+        this->pm_10_0_std_sensor_->publish_state(pm_10_0_std_concentration);
+ */
+}
+
+
+/* void vesync::handle_char_(uint8_t c) {
+  if (c == '\r')
+    return;
+  if (c == '\n') {
+    std::string s(this->rx_message_.begin(), this->rx_message_.end());
+    if (this->the_text_ != nullptr)
+      this->the_text_->publish_state(s);
+    if (this->the_sensor_ != nullptr)
+      this->the_sensor_->publish_state(parse_number<float>(s).value_or(0));
+    if (this->the_binsensor_ != nullptr)
+      this->the_binsensor_->publish_state(s == "ON");
+    this->rx_message_.clear();
+    return;
+  }
+  this->rx_message_.push_back(c);
+} */
+
+void vesync::dump_config() {
+//  LOG_TEXT_SENSOR("", "The Text Sensor", this->the_text_);
+  LOG_SENSOR("", "The Sensor", this->vesyncPowerSwitch_);
+}
+
+void vesync::send_command_onoff(bool state) {
+  this->data_index_ = 0;
+  this->data_[data_index_++] = 0xA5;
+  this->data_[data_index_++] = 0x22;
+  this->data_[data_index_++] = 0x09; // might need 0x0C for off?
+  this->data_[data_index_++] = 0x05;
+  this->data_[data_index_++] = 0x00;
+  this->data_[data_index_++] = 0x88; // might need 0x86 for off?
+  this->data_[data_index_++] = 0x01;
+  this->data_[data_index_++] = 0x00;
+  this->data_[data_index_++] = 0xA0;
+  this->data_[data_index_++] = 0x00;
+  if (state) {
+    this->data_[data_index_++] = 0x01;
+  } else {
+    this->data_[data_index_++] = 0x00;
+  }
+  for (int i = 0; i < data_index_; i++) {
+    this->parent_->write_byte(this->data_[i]);
+  }
+  this->data_index_ = 0;
+}
+
+
+/*
+void vesync::write_binary(bool state) {
+  this->write_str(ONOFF(state));
+}
+
+void UARTDemo::ping() {
+  this->write_str("PING");
+}
+
+void UARTDemo::write_float(float state) {
+  this->write_str(to_string(state).c_str());
+}
+
+void UARTDemoBOutput::dump_config() {
+  LOG_BINARY_OUTPUT(this);
+}
+
+void UARTDemoBOutput::write_state(bool state) {
+  this->parent_->write_binary(state);
+}
+
+void UARTDemoFOutput::dump_config() {
+  LOG_FLOAT_OUTPUT(this);
+}
+
+void UARTDemoFOutput::write_state(float state) {
+  this->parent_->write_float(state);
+}
+
+void UARTDemoSwitch::dump_config() {
+  LOG_SWITCH("", "UART Demo Switch", this);
+}
+
+void UARTDemoSwitch::write_state(bool state) {
+  this->parent_->write_binary(state);
+  this->publish_state(state);
+}
+
+void UARTDemoButton::dump_config() {
+  LOG_BUTTON("", "UART Demo Button", this);
+}
+
+void UARTDemoButton::press_action() {
+  this->parent_->ping();
+}
+*/
+
+}  // namespace uart_demo
+}  // namespace esphome
